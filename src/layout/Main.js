cc8c055add7abbdd2319fc06960672840bf8aacc
Brav0 <rybinil1998@gmail.com>
2022-09-29 15:32:28 +0300

    Refactoring to functional components

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,40 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Movies } from "../components/Movies";
 import { Search } from "../components/Search";
 import { Preloader } from '../components/Preloader';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-  state = {
-    movies:[],
-    loading: true,
-  };
-
-  componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-      .then(response => response.json())
-      .then(data => this.setState({movies: data.Search, loading: false}))  
-  }
-
-  searchMovies = (str, type = 'all') => {
-    this.setState({loading: true});
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  const searchMovies = (str, type = 'all') => {
+    setLoading(true);
     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then(response => response.json())
-      .then(data => this.setState({movies: data.Search, loading: false})) 
+      .then(data => {
+        setMovies(data.Search)
+        setLoading(false)
+      }) 
   }
 
-  render() {
-    const {movies, loading} = this.state;
-    return(
-      <main className="container content">
-        <Search searchMovies={this.searchMovies}/>
-        {
-          loading ? (<Preloader/>) : (<Movies movies={movies}/>)
-        }
-      </main>
-    )
-  };
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    .then(response => response.json())
+    .then(data => {
+      setMovies(data.Search)
+      setLoading(false)
+    })  
+  }, [])
+
+  return(
+    <main className="container content">
+      <Search searchMovies={searchMovies}/>
+      {
+        loading ? (<Preloader/>) : (<Movies movies={movies}/>)
+      }
+    </main>
+  )
 }
 
 export { Main };
